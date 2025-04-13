f480f661ee92054362540d3b1c8bd5a99728315c
vestyx <89847307+vestyx@users.noreply.github.com>
2025-04-13 10:04:29 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,10 @@
 local Scripts = {
-    [85896571713843] = "https://raw.githubusercontent.com/vestyx/VestyHub/refs/heads/main/85896571713843", -- Bubble Gum Simulator INFINITY
+    [85896571713843] = "https://raw.githubusercontent.com/vestyx/VestyHub/refs/heads/main/Bubble%20Gum%20Simulator%20INFINITY", -- Bubble Gum Simulator INFINITY
 }
 
 local scriptURL = Scripts[game.PlaceId]
 if scriptURL then
-    loadstring(game:HttpGet(scriptURL, true))()
+    (loadstring or load)(game:HttpGet(scriptURL, true))()
 else
     warn("No script available for this game.")
 end
