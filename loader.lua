f94a4e9fbc654c12d5c09f89f94421163a74537e
vestyx <89847307+vestyx@users.noreply.github.com>
2025-04-19 13:18:30 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,8 @@
 local Scripts = {
     [85896571713843] = "https://raw.githubusercontent.com/vestyx/VestyHub/refs/heads/main/Bubble%20Gum%20Simulator%20INFINITY", -- Bubble Gum Simulator INFINITY
+    [18901165922] = "https://raw.githubusercontent.com/vestyx/VestyHub/refs/heads/main/petsgoloader.lua", -- PETS GO! Overworld
+    [19006211286] = "https://raw.githubusercontent.com/vestyx/VestyHub/refs/heads/main/petsgoloader.lua", -- PETS GO! Plaza
+    [133783083257328] = "https://raw.githubusercontent.com/vestyx/VestyHub/refs/heads/main/petsgoloader.lua", -- PETS GO! Plaza Pro
 }
 
 local scriptURL = Scripts[game.PlaceId]
